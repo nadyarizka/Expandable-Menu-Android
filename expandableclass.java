0bb13ea99019d7709bed1d0f1980366831c53537
nadyarizka <maharaninadya@gmail.com>
2015-12-12 16:28:37 +0700

    Expandable Menu
    belum di rapiin..tapi kode kasarannya kyk gitu

---
+++ expandableclass.java
@@ -0,0 +1,142 @@
+package papb.coba.parkinsonkit;
+
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+/**
+ * Created by NADYA ARIZKA on 12/12/2015.
+ */
+public class expandableclass extends Activity {
+    LinearLayout mLinearLayout;
+    LinearLayout mLinearLayoutHeader;
+    ValueAnimator mAnimator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_information_komunitas);
+
+        //button call
+        ((Button)findViewById(R.id.call_YPI)).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent callYPI = new Intent(Intent.ACTION_DIAL);
+                callYPI.setData(Uri.parse("tel:08127411663"));
+                startActivity(callYPI);
+
+            }
+        });
+
+        mLinearLayout = (LinearLayout) findViewById(R.id.expandable);
+        //mLinearLayout = (View.OnClickListener) findViewById(R.id.expandable);
+        //mLinearLayout.setVisibility(View.GONE);
+        mLinearLayoutHeader = (LinearLayout) findViewById(R.id.header);
+
+        //Add onPreDrawListener
+        mLinearLayout.getViewTreeObserver().addOnPreDrawListener(
+                new ViewTreeObserver.OnPreDrawListener() {
+
+                    @Override
+                    public boolean onPreDraw() {
+                        mLinearLayout.getViewTreeObserver().removeOnPreDrawListener(this);
+                        mLinearLayout.setVisibility(View.GONE);
+
+                        final int widthSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+                        final int heightSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+                        mLinearLayout.measure(widthSpec, heightSpec);
+
+                        mAnimator = slideAnimator(0, mLinearLayout.getMeasuredHeight());
+                        return true;
+                    }
+                });
+
+
+        mLinearLayoutHeader.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (mLinearLayout.getVisibility() == View.GONE) {
+                    expand();
+                } else {
+                    collapse();
+                }
+            }
+        });
+    }
+
+
+    private void expand() {
+        //set Visible
+        mLinearLayout.setVisibility(View.VISIBLE);
+
+		/* Remove and used in preDrawListener
+        final int widthSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+		final int heightSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+		mLinearLayout.measure(widthSpec, heightSpec);
+		mAnimator = slideAnimator(0, mLinearLayout.getMeasuredHeight());
+		*/
+
+        mAnimator.start();
+    }
+
+    private void collapse() {
+        int finalHeight = mLinearLayout.getHeight();
+
+        ValueAnimator mAnimator = slideAnimator(finalHeight, 0);
+
+        mAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                //Height=0, but it set visibility to GONE
+                mLinearLayout.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationStart(Animator animator) {
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+            }
+        });
+        mAnimator.start();
+    }
+
+
+    private ValueAnimator slideAnimator(int start, int end) {
+
+        ValueAnimator animator = ValueAnimator.ofInt(start, end);
+
+
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                //Update Height
+                int value = (Integer) valueAnimator.getAnimatedValue();
+
+                ViewGroup.LayoutParams layoutParams = mLinearLayout.getLayoutParams();
+                layoutParams.height = value;
+                mLinearLayout.setLayoutParams(layoutParams);
+            }
+        });
+        return animator;
+    }
+
+
+}
